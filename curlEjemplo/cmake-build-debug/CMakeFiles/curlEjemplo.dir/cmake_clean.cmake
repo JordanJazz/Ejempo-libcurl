85ea27eef10f750dea091fb798e919e3e3e2c426
JordanJazz <80367259+JordanJazz@users.noreply.github.com>
2021-06-13 15:18:38 -0600

    abrir desde el ejemplo

---
+++ curlEjemplo/cmake-build-debug/CMakeFiles/curlEjemplo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curlEjemplo.dir/main.cpp.obj"
+  "curlEjemplo.exe"
+  "curlEjemplo.exe.manifest"
+  "curlEjemplo.pdb"
+  "libcurlEjemplo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curlEjemplo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
